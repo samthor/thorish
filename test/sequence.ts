7946ea2287c6120f9ec226c14ef5989dae249e40
Sam Thorogood <sam.thorogood@gmail.com>
2024-09-14 07:41:56 +1000

    fix test

--- test/sequence.ts
+++ test/sequence.ts
@@ -1,20 +1,40 @@
 import test from 'node:test';
 import * as assert from 'node:assert';
 import { buildSequencer } from '../src/sequence.js';
+import { timeout } from '../src/promise.js';
 
-test('buildSequenceListener', () => {
+test('buildSequenceListener', async () => {
+  const c = new AbortController();
   const { addListener, notify } = buildSequencer<number>();
 
-  const values: number[] = [];
+  const values: number[][] = [];
 
-  addListener(async (value, next) => {
-    values.push(value);
-    const v2 = await next();
-    values.push(v2);
-  });
+  addListener(
+    async (value, next) => {
+      const out: number[] = [];
+      values.push(out);
+
+      out.push(value);
+      for (;;) {
+        const v2 = await next();
+        out.push(v2);
+      }
+    },
+    { signal: c.signal },
+  );
 
   notify(1);
   notify(2);
+  await timeout(1);
 
-  assert.deepStrictEqual(values, [1, 2]);
+  assert.deepStrictEqual(values, [[1, 2], [2]]);
+
+  // first two runs will still be active - won't have 3rd
+  c.abort();
+  notify(3);
+  await timeout(1);
+  assert.deepStrictEqual(values, [
+    [1, 2, 3],
+    [2, 3],
+  ]);
 });
